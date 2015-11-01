a52af26d03d9ce2a848248e7c07ffee04c8be428
liamrahav <liorogamerdvd@gmail.com>
2015-11-01 16:15:25 -0500

    Functionality basically complete

--- run.py
+++ run.py
@@ -1,13 +1,33 @@
-#!/usr/bin/python
+# Created on 11/1/15 by Liam Rahav
 
-#  Created on 11/1/15 by Liam Rahav
-
-import sys
+import argparse
+import os
 from imgurpython import ImgurClient
 from keys import CLIENT_ID, CLIENT_SECRET
+from getopt import getopt
 
-imgur = ImgurClient(CLIENT_ID, CLIENT_SECRET)
-items = imgur.gallery()
-for item in items:
-    print(item.link)
+
+def create_parser():
+    p = argparse.ArgumentParser(description="Upload image to imgur")
+    p.add_argument('filename', type=str, help="The filename of the image to be uploaded")
+    return p
+
+
+def upload_image():
+    print("Starting upload...")
+    image = imgur.upload_from_path(
+        os.path.dirname(os.path.realpath(args.filename) + "/" + args.filename)
+    )
+    print("Done!")
+    return image
+
+
+if __name__ == '__main__':
+    parser = create_parser()
+    args = parser.parse_args()
+    imgur = ImgurClient(CLIENT_ID, CLIENT_SECRET)
+    image = upload_image()
+    print("URL: \033[94m" + image['link'] + "\033[0m")
+
+
 
